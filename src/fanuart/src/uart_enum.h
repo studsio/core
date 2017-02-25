54c9c6211df3cc0afc6b8214b9fdd93fec524e64
Andy Frank <afrankvt@gmail.com>
2017-02-25 16:12:32 -0500

    Fix uart_xxx copyright headers

--- src/fanuart/src/uart_enum.h
+++ src/fanuart/src/uart_enum.h
@@ -1,13 +1,18 @@
 /*
-// Copyright (c) 2017, Andy Frank
-// Licensed under the Academic Free License version 3.0
-//
-// History:
-//   24 Jan 2017  Andy Frank  Creation
-//
-// Based on nerves_uart by Frank Hunleth:
-// https://github.com/nerves-project/nerves_uart
-*/
+ *  Copyright 2016 Frank Hunleth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef UART_ENUM_H
 #define UART_ENUM_H
